794251b0c72a53d8dd9b2f6087ea75d6778924b0
Luskans <sylvain.migne.pro@gmail.com>
2024-03-05 16:45:13 +0100

    fix import url for utils

--- src/utils/getImageUrl.js
+++ src/utils/getImageUrl.js
@@ -1,5 +1,8 @@
 const apiURL = import.meta.env.VITE_API_URL;
-const url = apiURL.replace('/api', '');
+let url;
+if (apiURL) {
+    url = apiURL.replace('/api', '');
+}
 
 export default function getImageUrl(storyName) {
     return `${url}/images/${storyName}`
